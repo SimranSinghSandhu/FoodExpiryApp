a112ace413e0ee61fd595bce42d61f152205e743
Simran <sam.sandhu131@gmail.com>
2020-06-21 16:39:35 +0530

    First Commit

---
+++ FoodExpiryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodExpiryApp
+//
+//  Created by Simran Singh Sandhu on 21/06/20.
+//  Copyright © 2020 Simran Singh Sandhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
